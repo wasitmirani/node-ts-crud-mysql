ac0f8d2c87c343852773ce74c09aa84853ec9f4a
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2023-05-18 01:46:48 +0500

    jwt auth

--- routes/api.ts
+++ routes/api.ts
@@ -25,7 +25,7 @@ route.get('/', function (req, res) {
 
   let val= new validate();
   route.get('/users',user_controller.getUsers);
-  route.post('/store-user',user_controller.storeUser);
+  route.post('/store-user',val.signupValidation,user_controller.storeUser);
   route.get('/user/:id',user_controller.findUser);
 // Export the router
 export = route;
